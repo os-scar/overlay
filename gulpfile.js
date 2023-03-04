a77144331f45414d3c34e28697444ca30e88259d
Guy Nachshon <58976716+GuyNachshon@users.noreply.github.com>
2023-03-04 21:24:44 +0200

    feat: indicator component tooltip + css file (#11)
    Co-authored-by: jossef <jossef12@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,23 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 import replace from 'gulp-replace';
 import rename from 'gulp-rename';
-import {rollup} from 'rollup';
-import * as vite from 'vite'
-import svgLoader from 'vite-svg-loader'
-import vue from '@vitejs/plugin-vue'
+
+import { rollup } from 'rollup';
+import * as vite from 'vite';
+import svgLoader from 'vite-svg-loader';
+import vue from '@vitejs/plugin-vue';
+import { promises as fs } from 'fs';
 
 import commonjs from '@rollup/plugin-commonjs';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 import * as path from 'path';
 import * as url from 'url';
 import * as del from 'del';
 
 const DEV_VERSION = '0.0.1';
 const FILE_EXTENSION_ZIP = 'zip';
-const FILE_EXTENSION_XPI = "xpi";
+const FILE_EXTENSION_XPI = 'xpi';
 
 const BROWSER_TYPE_CHROME = 'chrome';
 const BROWSER_TYPE_FIREFOX = 'firefox';
@@ -25,103 +28,129 @@ let scriptDirPath = path.dirname(scriptFilePath);
 let srcDirPath = path.resolve(scriptDirPath, 'src');
 let distDirPath = path.resolve(scriptDirPath, 'dist');
 
-async function buildCustomElements(outputDirPath){
+async function buildCustomElements(outputDirPath) {
+  await vite.build({
+    build: {
+      emptyOutDir: false,
+      outDir: outputDirPath,
+      lib: {
+        entry: path.join(srcDirPath, 'custom-elements', 'index.js'),
+        name: 'custom-elements',
+        formats: ['es'],
+        fileName: 'custom-elements',
+      },
+    },
+    define: {
+      'process.env': {},
+    },
+    plugins: [vue({ customElement: true }), svgLoader()],
+  });
 
-    await vite.build({
-        build: {
-            emptyOutDir: false,
-            outDir: outputDirPath,
-            lib: {
-                entry: path.join(srcDirPath, 'custom-elements', 'index.js'),
-                name: 'custom-elements',
-                formats: ['es'],
-                fileName: 'custom-elements',
-            },
-        },
-        define: {
-            'process.env': {}
-        },
-        plugins: [vue({customElement: true}), svgLoader()]
-    });
+  // --------------
+  // Recompile without customElement: true to extract css
+  let results = await vite.build({
+    build: {
+      emptyOutDir: false,
+      write: false,
+      lib: {
+        entry: path.join(srcDirPath, 'custom-elements', 'index.js'),
+        name: 'custom-elements',
+        fileName: 'custom-elements',
+      },
+    },
+    plugins: [vue(), svgLoader()],
+  });
 
+  let customElementsCssOutputFilePath = path.join(distDirPath, 'custom-elements.css');
+
+  let files = results[0].output;
+  for (const file of files) {
+    if (file.name !== 'style.css') {
+      continue;
+    }
+    let cssFileContent = file.source;
+    await fs.writeFile(customElementsCssOutputFilePath, cssFileContent, 'utf8');
+  }
 }
 
 async function buildBrowserExtension(browserType, version, fileExtension) {
-    let outputDirPath = path.join(distDirPath, browserType);
+  let outputDirPath = path.join(distDirPath, browserType);
 
-    // --------------
-    // icons dir
-    await gulp.src(path.join(scriptDirPath, 'icons', '**', '*'))
-        .pipe(gulp.dest(path.join(outputDirPath, 'icons')))
+  // --------------
+  // icons dir
+  await gulp.src(path.join(scriptDirPath, 'icons', '**', '*')).pipe(gulp.dest(path.join(outputDirPath, 'icons')));
 
-    // --------------
-    // custom-elements.js
-    await gulp.src(path.join(distDirPath, 'custom-elements.js'))
-        .pipe(gulp.dest(path.join(outputDirPath)))
+  // --------------
+  // custom-elements.js + css
+  await gulp.src(path.join(distDirPath, 'custom-elements.js')).pipe(gulp.dest(path.join(outputDirPath)));
+  await gulp.src(path.join(distDirPath, 'custom-elements.css')).pipe(gulp.dest(path.join(outputDirPath)));
 
-    // --------------
-    // manifest.json
-    let manifestFilename = `manifest.${browserType}.json`;
-    await gulp.src(path.join(srcDirPath, manifestFilename))
-        .pipe(rename('manifest.json'))
-        .pipe(replace('{{EXTENSION_VERSION}}', version))
-        .pipe(replace('{{EXTENSION_DESCRIPTION}}', 'description'))
-        .pipe(gulp.dest(outputDirPath))
+  // --------------
+  // manifest.json
+  let manifestFilename = `manifest.${browserType}.json`;
+  await gulp
+    .src(path.join(srcDirPath, manifestFilename))
+    .pipe(rename('manifest.json'))
+    .pipe(replace('{{EXTENSION_VERSION}}', version))
+    .pipe(replace('{{EXTENSION_DESCRIPTION}}', 'description'))
+    .pipe(gulp.dest(outputDirPath));
 
-    // --------------
-    // content.stackoverflow.js
-    let bundle = await rollup({
-        input: path.join(srcDirPath, 'content.stackoverflow.js')
-    });
-    await bundle.write({
-        file: path.join(outputDirPath, 'content.stackoverflow.js'),
-        format: 'iife',
-    })
+  // --------------
+  // content.stackoverflow.js
+  let bundle = await rollup({
+    input: path.join(srcDirPath, 'content.stackoverflow.js'),
+  });
+  await bundle.write({
+    file: path.join(outputDirPath, 'content.stackoverflow.js'),
+    format: 'iife',
+  });
 
-    // --------------
-    // content.npm.js
-    bundle = await rollup({
-        input: path.join(srcDirPath, 'content.npm.js')
-    });
-    await bundle.write({
-        file: path.join(outputDirPath, 'content.npm.js'),
-        format: 'iife',
-    })
+  // --------------
+  // content.npm.js
+  bundle = await rollup({
+    input: path.join(srcDirPath, 'content.npm.js'),
+  });
+  await bundle.write({
+    file: path.join(outputDirPath, 'content.npm.js'),
+    format: 'iife',
+  });
 
-    // --------------
-    // background.js
-    bundle = await rollup({
-        input: path.join(srcDirPath, 'background.js'),
-        plugins: [commonjs(), nodeResolve()],
-    });
-    await bundle.write({
-        file: path.join(outputDirPath, 'background.js'),
-        format: 'iife',
-    })
+  // --------------
+  // background.js
+  bundle = await rollup({
+    input: path.join(srcDirPath, 'background.js'),
+    plugins: [commonjs(), nodeResolve()],
+  });
+  await bundle.write({
+    file: path.join(outputDirPath, 'background.js'),
+    format: 'iife',
+  });
 
-    // --------------
-    // Zip {browserName}.zip
-    await gulp.src(path.join(outputDirPath, '**', '*'))
-        .pipe(zip(`${browserType}_${version}.${fileExtension}`))
-        .pipe(gulp.dest(distDirPath))
+  // --------------
+  // Zip {browserName}.zip
+  await gulp
+    .src(path.join(outputDirPath, '**', '*'))
+    .pipe(zip(`${browserType}_${version}.${fileExtension}`))
+    .pipe(gulp.dest(distDirPath));
 }
-gulp.task('compile', async () => {
-    let version = process.env['BUILD_VERSION'] || DEV_VERSION;
-    console.log(`compiling version ${version}`)
 
-    await buildCustomElements(distDirPath)
-    await buildBrowserExtension(BROWSER_TYPE_CHROME, version, FILE_EXTENSION_ZIP);
-    await buildBrowserExtension(BROWSER_TYPE_FIREFOX, version, FILE_EXTENSION_XPI);
+gulp.task('compile', async () => {
+  let version = process.env['BUILD_VERSION'] || DEV_VERSION;
+  console.log(`compiling version ${version}`);
+
+  await buildCustomElements(distDirPath);
+  await buildBrowserExtension(BROWSER_TYPE_CHROME, version, FILE_EXTENSION_ZIP);
+  await buildBrowserExtension(BROWSER_TYPE_FIREFOX, version, FILE_EXTENSION_XPI);
 });
 
 gulp.task('clean', async () => {
-    await del.deleteAsync(distDirPath)
-})
+  await del.deleteAsync(distDirPath);
+});
 
 gulp.task('build', gulp.series('clean', 'compile'));
 
 gulp.task('watch', function () {
-    gulp.watch('./src/**/*', gulp.series('build'));
-})
+  gulp.watch('./src/**/*', gulp.series('build'));
+});
 
 gulp.task('default', gulp.series('build'));
--- src/custom-elements/indicator.vue
+++ src/custom-elements/indicator.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="overlay-indicator" :class="{ 'overlay-indicator--issues': package.issues ?? length }">
+  <div
+    class="overlay-indicator"
+    :class="{ 'overlay-indicator--issues': package.issues ?? length }"
+    @mouseenter="
+      initTooltipPosition();
+      this.overIndicator = true;
+      tooltipOpen = true;
+    "
+    @mouseleave="
+      this.overIndicator = false;
+      shouldCloseTooltip();
+    "
+    ref="overlay"
+  >
     <div class="overlay-indicator__icon">{{ package.issues }}</div>
     <div class="overlay-indicator__text">
       <slot></slot>
     </div>
+    <teleport to="body" append-to="self">
+      <div
+        class="overlay-indicator__tooltip"
+        v-show="tooltipOpen"
+        @mouseenter="overTooltip = true"
+        @mouseleave="
+          overTooltip = false;
+          shouldCloseTooltip();
+        "
+        ref="overlayTooltip"
+      >
+        <!-- test to open url in new page svg-->
+        <a target="_blank" :href="`https://www.npmjs.com/package/${package.name}`">{{ package.name }}</a>
+
+        <!-- test to load svg-->
+        <component :is="`${package.type}_logo`"></component>
+
+        <!-- just show all package json at the moment-->
+        <div>{{ package }}</div>
+      </div>
+    </teleport>
   </div>
 </template>
 
 <script>
+import npm_logo from './assets/npm_logo.svg?component';
+
+const TOOLTIP_POSITION = {
+  TOP_START: 'top_start',
+  TOP_END: 'top_end',
+  TOP: 'top',
+  BOTTOM_START: 'bottom_start',
+  BOTTOM_END: 'bottom_end',
+  BOTTOM: 'bottom',
+  LEFT_START: 'left_start',
+  LEFT_END: 'left_end',
+  LEFT: 'left',
+  RIGHT_START: 'right_start',
+  RIGHT_END: 'right_end',
+  RIGHT: 'right',
+};
+
 export default {
   name: 'overlay-indicator',
   props: {
@@ -19,7 +70,11 @@ export default {
     },
   },
   data() {
-    return {};
+    return {
+      tooltipOpen: false,
+      overTooltip: false,
+      overIndicator: false,
+    };
   },
   computed: {
     store() {
@@ -29,14 +84,195 @@ export default {
       return `${this.overlayIndicatorPackageType}/${this.overlayIndicatorPackageName}`;
     },
     package() {
-      return this.store.packages?.[this.packageId] || {};
+      return this.store.packages[this.packageId] || {};
     },
   },
-  methods: {},
+  methods: {
+    initTooltipPosition() {
+      let baseElement = this.$refs.overlay,
+        tooltipElement = this.$refs.overlayTooltip,
+        tooltipPosition = TOOLTIP_POSITION.BOTTOM;
+      this.placeTooltip(baseElement, tooltipElement, tooltipPosition);
+    },
+    placeTooltip(baseElement, tooltipElement, tooltipPosition) {
+      const baseRect = baseElement.getBoundingClientRect();
+      const tooltipRect = tooltipElement.getBoundingClientRect() || {};
+      const parent = baseElement.getRootNode().host || document;
+      let targetPosition = this.getPosition(baseRect, tooltipRect, tooltipPosition);
+      const parentRect = parent === document ? this.getDocumentBoundingRect() : parent.getBoundingClientRect();
+      if (this.shouldFlip(targetPosition, tooltipRect, parentRect, tooltipPosition)) {
+        tooltipPosition = this.getOppositePosition(tooltipPosition);
+        targetPosition = this.getPosition(baseRect, tooltipRect, tooltipPosition);
+      }
+      tooltipElement.style.left = targetPosition.left + 'px';
+      tooltipElement.style.top = targetPosition.top + 'px';
+    },
+    getPosition(baseRect, tooltipRect, tooltipPosition) {
+      const basePadding = 4;
+      tooltipRect.width = tooltipRect.width || 260;
+      tooltipRect.height = tooltipRect.height || 200;
+      let pageOffset = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      switch (tooltipPosition) {
+        case TOOLTIP_POSITION.TOP_START:
+          return {
+            top: pageOffset + baseRect.top - basePadding - tooltipRect.height,
+            left: baseRect.left,
+          };
+        case TOOLTIP_POSITION.TOP_END:
+          return {
+            top: pageOffset + baseRect.top - basePadding - tooltipRect.height,
+            left: baseRect.left - baseRect.width - tooltipRect.width - basePadding,
+          };
+        case TOOLTIP_POSITION.TOP:
+          return {
+            top: pageOffset + baseRect.top - tooltipRect.height,
+            left: baseRect.left + baseRect.width / 2 - tooltipRect.width / 2,
+          };
+        case TOOLTIP_POSITION.BOTTOM_START:
+          return {
+            top: pageOffset + baseRect.top + baseRect.height + basePadding,
+            left: baseRect.left + basePadding,
+          };
+        case TOOLTIP_POSITION.BOTTOM_END:
+          return {
+            top: pageOffset + baseRect.top + baseRect.height + baseRect.height + basePadding,
+            left: baseRect.left - baseRect.width - tooltipRect.width - basePadding,
+          };
+        case TOOLTIP_POSITION.BOTTOM:
+          if (baseRect.left + baseRect.width / 2 - tooltipRect.width / 2 < 0) {
+            return {
+              top: pageOffset + baseRect.top + baseRect.height + basePadding,
+              left: baseRect.left + baseRect.width / 2,
+            };
+          }
+          return {
+            top: pageOffset + baseRect.top + baseRect.height + basePadding,
+            left: baseRect.left + baseRect.width / 2 - tooltipRect.width / 2,
+          };
+        case TOOLTIP_POSITION.LEFT_START:
+          return {
+            top: pageOffset + baseRect.top + basePadding,
+            left: baseRect.left - tooltipRect.width - basePadding,
+          };
+        case TOOLTIP_POSITION.LEFT_END:
+          return {
+            top: pageOffset + baseRect.top - baseRect.height - tooltipRect.height - basePadding,
+            left: baseRect.left - tooltipRect.width - basePadding,
+          };
+        case TOOLTIP_POSITION.LEFT:
+          return {
+            top: pageOffset + baseRect.top + baseRect.height / 2 - tooltipRect.height / 2,
+            left: baseRect.left - tooltipRect.width - basePadding,
+          };
+        case TOOLTIP_POSITION.RIGHT_START:
+          return {
+            top: pageOffset + baseRect.top,
+            left: baseRect.left + baseRect.width + basePadding,
+          };
+        case TOOLTIP_POSITION.RIGHT_END:
+          return {
+            top: pageOffset + baseRect.top - baseRect.height - tooltipRect.height - basePadding,
+            left: baseRect.left + baseRect.width,
+          };
+        case TOOLTIP_POSITION.RIGHT:
+          return {
+            top: pageOffset + baseRect.top + baseRect.height / 2 - tooltipRect.height / 2,
+            left: baseRect.left + baseRect.width + basePadding,
+          };
+        default:
+          return {
+            top: pageOffset + baseRect.top + baseRect.height + basePadding,
+            left: baseRect.left + baseRect.width + basePadding,
+          };
+      }
+    },
+    getOppositePosition(position) {
+      switch (position) {
+        case TOOLTIP_POSITION.TOP_START:
+          return TOOLTIP_POSITION.BOTTOM_START;
+        case TOOLTIP_POSITION.TOP_END:
+          return TOOLTIP_POSITION.BOTTOM_END;
+        case TOOLTIP_POSITION.TOP:
+          return TOOLTIP_POSITION.BOTTOM;
+        case TOOLTIP_POSITION.BOTTOM_START:
+          return TOOLTIP_POSITION.TOP_START;
+        case TOOLTIP_POSITION.BOTTOM_END:
+          return TOOLTIP_POSITION.TOP_END;
+        case TOOLTIP_POSITION.BOTTOM:
+          return TOOLTIP_POSITION.TOP;
+        case TOOLTIP_POSITION.LEFT_START:
+          return TOOLTIP_POSITION.RIGHT_START;
+        case TOOLTIP_POSITION.LEFT_END:
+          return TOOLTIP_POSITION.RIGHT_END;
+        case TOOLTIP_POSITION.LEFT:
+          return TOOLTIP_POSITION.RIGHT;
+        case TOOLTIP_POSITION.RIGHT_START:
+          return TOOLTIP_POSITION.LEFT_START;
+        case TOOLTIP_POSITION.RIGHT_END:
+          return TOOLTIP_POSITION.LEFT_END;
+        case TOOLTIP_POSITION.RIGHT:
+          return TOOLTIP_POSITION.LEFT;
+        default:
+          return TOOLTIP_POSITION.BOTTOM;
+      }
+    },
+    getDocumentBoundingRect() {
+      const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+      const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+      return {
+        top: 0,
+        left: 0,
+        right: width,
+        bottom: height,
+        width: width,
+        height: height,
+      };
+    },
+    shouldFlip(targetPosition, tooltipRect, boundaryRect, position) {
+      tooltipRect.width = tooltipRect.width || 260;
+      tooltipRect.height = tooltipRect.height || 440;
+      let pageOffset = window.scrollY || document.documentElement.scrollTop;
+      let pageBoundaryBottom = pageOffset + window.innerHeight;
+      switch (position) {
+        case TOOLTIP_POSITION.TOP_START:
+        case TOOLTIP_POSITION.TOP_END:
+        case TOOLTIP_POSITION.TOP:
+          return pageOffset + targetPosition.top > pageOffset.top;
+        case TOOLTIP_POSITION.BOTTOM_START:
+        case TOOLTIP_POSITION.BOTTOM_END:
+        case TOOLTIP_POSITION.BOTTOM:
+          return pageBoundaryBottom < targetPosition.top + tooltipRect.height;
+        case TOOLTIP_POSITION.LEFT_START:
+        case TOOLTIP_POSITION.LEFT_END:
+        case TOOLTIP_POSITION.LEFT:
+          return targetPosition.left > boundaryRect.left;
+        case TOOLTIP_POSITION.RIGHT_START:
+        case TOOLTIP_POSITION.RIGHT_END:
+        case TOOLTIP_POSITION.RIGHT:
+          return targetPosition.left + tooltipRect.width < boundaryRect.right;
+        default:
+          return false;
+      }
+    },
+    shouldCloseTooltip() {
+      setTimeout(() => {
+        if (!this.overTooltip && !this.overIndicator) {
+          this.tooltipOpen = false;
+        }
+      }, 300);
+    },
+  },
+  mounted() {
+    this.initTooltipPosition();
+  },
+  components: {
+    npm_logo,
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $font-family: 'Lexend', sans-serif;
 $font-size-title: 12px;
 $font-size-score-value: 10px;
@@ -52,21 +288,18 @@ $color-red: #ff0000;
 $color-green: #14ce00;
 $color-white: #fff;
 
-div {
-  box-sizing: border-box;
-}
-
 $indicator-height: 24px;
 
 .overlay-indicator {
   display: inline-flex;
   flex-direction: row;
+  position: relative;
   height: $indicator-height;
   overflow: hidden;
   border-radius: 4px;
   align-items: center;
+  z-index: 10;
   box-shadow: 0 0 0 1px $color-black;
-
   $class-name: &;
 
   &#{$class-name}--issues {
@@ -94,8 +327,17 @@ $indicator-height: 24px;
   }
 
   &__tooltip {
+    position: absolute;
     padding: 20px;
-    background: #eee;
+    z-index: 1000;
+    background: #e12d33;
+    width: 260px;
+    height: 200px;
+    overflow: scroll;
+  }
+
+  div {
+    box-sizing: border-box;
   }
 }
 </style>
