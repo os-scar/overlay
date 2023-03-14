241c2af605d117467f514625c4f1787c4e39bc43
Baruch Odem (Rothkoff) <baruchiro@gmail.com>
2023-03-14 13:50:30 +0200

    fix: inject CSS to the webpage (#23)
    - rename component filename
    - inject custom-elements.css to the webpage

--- src/content.js
+++ src/content.js
@@ -4,8 +4,13 @@ const injectScriptTag = () => {
   const script = document.createElement('script');
   script.src = chrome.runtime.getURL('custom-elements.js');
   (document.head || document.documentElement).appendChild(script);
-
   console.log('Injected script tag', script);
+
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = chrome.runtime.getURL('custom-elements.css');
+  (document.head || document.documentElement).appendChild(link);
+  console.log('Injected link tag', link);
 };
 
 export const mountContentScript = (contentScript) => {
--- src/custom-elements/index.js
+++ src/custom-elements/index.js
@@ -1,6 +1,6 @@
 import '@webcomponents/custom-elements';
 import { defineCustomElement } from 'vue';
-import indicator from './indicator.vue';
+import indicator from './Indicator.vue';
 import { initEventListenersAndStore } from './webapp-events';
 
 initEventListenersAndStore();
