247beef2fe44561c248da6180f0a835ebab13e6e
Baruch Odem <baruchiro@gmail.com>
2023-03-16 14:24:53 +0200

    cache with ttl and size limit Fixes #25

--- src/background/cache.js
+++ src/background/cache.js
@@ -1,16 +1,44 @@
 const _cache = {};
 
-const buildNewKeys = (keys) =>
-  keys.reduce((acc, currentKey) => {
-    if (!acc[currentKey]) {
-      acc[currentKey] = {};
+function cleanByTTL() {
+  const now = Date.now();
+  Object.keys(_cache).forEach((key) => {
+    if (_cache[key].ttl && now > _cache[key].ttl) {
+      delete _cache[key];
     }
-    return acc[currentKey];
-  }, _cache);
+  });
+}
 
-export default (keys, action) => {
-  const lastKey = typeof keys === 'string' ? keys : keys.pop();
-  const objectForKey = typeof keys === 'string' ? _cache : buildNewKeys(keys);
-  if (!objectForKey[lastKey]) objectForKey[lastKey] = action();
-  return objectForKey[lastKey];
+const cleanBySize = (size) => {
+  const keys = Object.keys(_cache);
+  if (keys.length > size) {
+    keys
+      .sort((a, b) => _cache[a].ttl - _cache[b].ttl)
+      .slice(0, size)
+      .forEach((key) => {
+        delete _cache[key];
+      });
+  }
+};
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+setInterval(() => {
+  cleanByTTL();
+  cleanBySize(1000);
+}, MINUTE);
+
+export default (keys, action, TTL = 15 * MINUTE) => {
+  const key = typeof keys === 'string' ? keys : keys.join('.');
+  if (!_cache[key])
+    _cache[key] = {
+      ttl: Date.now() + TTL,
+      value: action(),
+    };
+  return _cache[key].value;
+};
+
+export const forTests = {
+  cleanByTTL,
+  cleanBySize,
 };
--- src/background/cache.test.js
+++ src/background/cache.test.js
@@ -1,12 +1,17 @@
 /**
  * @jest-environment node
  */
-import { describe, expect, it, jest } from '@jest/globals';
-import cache from './cache';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import cache, { forTests } from './cache';
+const { cleanBySize, cleanByTTL } = forTests;
 
 const createPromise = (value) => new Promise((resolve) => setImmediate(() => resolve(value)));
 
 describe('cache', () => {
+  beforeEach(() => {
+    cleanBySize(0);
+  });
+
   it('should support one key without array', () => {
     const expected = 15;
     const actual = cache('key', () => expected);
@@ -51,14 +56,41 @@ describe('cache', () => {
     expect(actual).toBe(expected);
   });
 
-  it('should return object containing cached results', async () => {
+  it('should return undefined when key is sub-array', async () => {
     cache(['e', 'f', 'g'], () => 2);
     cache(['hi'], () => 2);
     cache(['e', 'f', 'h'], () => 2);
 
-    expect(cache(['e', 'f'])).toStrictEqual({
-      g: 2,
-      h: 2,
+    expect(cache(['e', 'f'], () => {})).toBeUndefined();
+  });
+
+  describe('cleaning', () => {
+    it('should remove expired keys by TTL', () => {
+      const expired = -1;
+      const notExpired = 1000 * 60 * 5;
+
+      cache(['expired'], () => 1, expired);
+      cache(['not-expired'], () => 2, notExpired);
+
+      cleanByTTL();
+
+      expect(cache(['expired'], () => {})).toBeUndefined();
+      expect(cache(['not-expired'], () => {})).toBe(2);
+    });
+
+    it('should remove the oldest TTLs', () => {
+      cache(['a'], () => 1, 1200);
+      cache(['b'], () => 2, 1100);
+      cache(['c'], () => 3, 1000);
+      cache(['d'], () => 4, 900);
+
+      cleanBySize(2);
+
+      expect(cache(['a'], () => {})).toBe(1);
+      expect(cache(['b'], () => {})).toBe(2);
+
+      expect(cache(['c'], () => {})).toBeUndefined();
+      expect(cache(['d'], () => {})).toBeUndefined();
     });
   });
 });
