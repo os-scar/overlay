27df2811ddba54efeae166449e828afece9ef519
Baruch Odem <baruch.odem@checkmarx.com>
2023-03-28 12:25:28 +0300

    Support `go get` commands to install from Github repo Fixes #46

--- src/content/registry/go.js
+++ src/content/registry/go.js
@@ -1,4 +1,49 @@
 export const parseCommand = (command = '') => {
-  console.log('go.js:parseCommand', command);
-  return [];
+  const packages = [];
+  let counterIndex = 0;
+
+  const lines = command.split('\n');
+  while (lines.length > 0) {
+    const line = lines.shift();
+
+    const goGetMatch = line.match(/go +(get|install)/);
+    if (!goGetMatch) {
+      counterIndex += line.length + 1; // +1 for the newline
+      continue;
+    }
+
+    const goGetLength = goGetMatch.index + goGetMatch[0].length;
+    counterIndex += goGetLength;
+    const argsAndPackagesWords = line.slice(goGetLength).split(' ');
+
+    while (argsAndPackagesWords.length > 0) {
+      const word = argsAndPackagesWords.shift();
+
+      if (!word) {
+        counterIndex++;
+        continue;
+      }
+
+      if (word.startsWith('-')) {
+        counterIndex += word.length + 1;
+        continue;
+      }
+
+      const packageMatch = word.match(/^(?<name>\w[\w./]+)(@[\w.]+)?$/);
+      if (!packageMatch) {
+        counterIndex += word.length + 1;
+        continue;
+      }
+
+      packages.push({
+        name: packageMatch.groups.name,
+        startIndex: counterIndex,
+        endIndex: counterIndex + packageMatch[0].length,
+      });
+
+      counterIndex += word.length + 1;
+    }
+  }
+
+  return packages;
 };
--- src/content/registry/go.test.js
+++ src/content/registry/go.test.js
@@ -1,24 +1,18 @@
 import { describe, expect, it } from '@jest/globals';
 import { parseCommand } from './go';
 
-const positionInCommand = (command, packageName) => {
+const positionInCommand = (command, packageName, endIndex) => {
   const startIndex = command.indexOf(packageName);
-  return { name: packageName, startIndex, endIndex: startIndex + packageName.length };
+  return { name: packageName, startIndex, endIndex: endIndex || startIndex + packageName.length };
 };
 
 describe(parseCommand.name, () => {
-  it.each([
-    undefined,
-    '',
-    'just a text',
-    'go get',
-    'go get -u',
-    'go get -u -v',
-    'go get https://github.com/user/package',
-    'go get code.google.com/p/goauth2/oauth',
-  ])(`should not find a package in '%s'`, (text) => {
-    expect(parseCommand(text)).toStrictEqual([]);
-  });
+  it.each([undefined, '', 'just a text', 'go get', 'go get -u', 'go get -u -v', 'go get https://github.com/user/package'])(
+    `should not find a package in '%s'`,
+    (text) => {
+      expect(parseCommand(text)).toStrictEqual([]);
+    }
+  );
 
   it.each(['github.com/user/package'])(`should find the package %s`, (packageName) => {
     const command = `go get ${packageName}`;
@@ -37,7 +31,16 @@ describe(parseCommand.name, () => {
     ['with version', 'go get github.com/user/package@v1.12.0'],
     ['with commit hash', 'go get github.com/user/package@b2bd9c3'],
   ])(`should find the package in command with '%s'`, (_, command) => {
-    const expectedPackages = [positionInCommand(command, 'github.com/user/package')];
+    const expectedPackages = [positionInCommand(command, 'github.com/user/package', command.length)];
+
+    const packagePosition = parseCommand(command);
+
+    expect(packagePosition).toStrictEqual(expectedPackages);
+  });
+
+  it('should find package from code.google.com', () => {
+    const command = 'go get code.google.com/p/goauth2/oauth';
+    const expectedPackages = [positionInCommand(command, 'code.google.com/p/goauth2/oauth')];
 
     const packagePosition = parseCommand(command);
 
