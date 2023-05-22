67e3e3d0ed0c286d446ef764bd08afa38ee87d93
Hagar Fisher <hagarfisher@gmail.com>
2023-05-22 23:08:50 +0300

    changing regex to match against npx and npm init

--- src/content/registry/npm.js
+++ src/content/registry/npm.js
@@ -1,12 +1,14 @@
 import { createParseCommand } from './shared';
 
-const npmInstall = /(npm|yarn)( -g)?( global)? (install|i|add|update) /;
+const npmInstall = /((npm|yarn)( -g)?( global)? (install|i|add|update))\s/;
+const npmInit = /(npx|npm init)\b/;
 const packageName = /(?<package_name>[a-z0-9_@][a-z0-9_./-]*)/;
 const packageVersion = /@(?<package_version>[~^]?\d+(\.(\d|X|x)+){0,2}(-[a-z0-9_-]+)?)/;
 const packageLabel = /@[a-z0-9_-]+/;
 const fullPackage = new RegExp(String.raw`^${packageName.source}(${packageVersion.source}|${packageLabel.source})?$`);
 
 const parsePackageString = (str) => {
+  console.log('hi');
   const match = str.match(fullPackage);
   if (!match) return null;
   return {
@@ -18,7 +20,7 @@ const parsePackageString = (str) => {
 
 export const parseCommand = createParseCommand(
   'npm',
-  (line) => line.match(npmInstall),
+  (line) => line.match(npmInstall) || line.match(npmInit),
   (word) => word.length + 1,
   parsePackageString
 );
