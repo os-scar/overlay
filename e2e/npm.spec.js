c92f8e03bd44f0d5fab9ed9a0c8ef575d5e10e82
Roy Zerbib <rzarviv@gmail.com>
2023-06-19 18:37:51 +0300

    fix: url change without window load on npm (#116)
    - created a new event: `EVENT_URL_CHANGED` that notifies content script
    on URL change
    - added a utility function :`waitForElement` that fires an action after
    a specified element is created
    - added tests
    
    close #107
    
    ---------
    
    Co-authored-by: Baruch Odem <baruch.odem@checkmarx.com>
    Co-authored-by: Ofer Lis <62609377+Oferlis@users.noreply.github.com>
    Co-authored-by: Baruch Odem (Rothkoff) <baruchiro@gmail.com>

--- e2e/npm.spec.js
+++ e2e/npm.spec.js
@@ -11,4 +11,51 @@ test.describe('npm', () => {
     const overlayModuleName = page.locator('.overlay-tooltip__tooltip .overlay-indicator__tooltip[data-testid="module_name"]');
     await Expect(overlayModuleName).toBeVisible();
   });
+
+  test('detect url change when using the searh bar in npmjs.com/package', async ({ page }) => {
+    let packageName = '@ngneat/spectator';
+    const url = `https://www.npmjs.com/package/${packageName}`;
+
+    await page.goto(url, {
+      waitUntil: 'domcontentloaded',
+    });
+
+    // get the package name from overlay popup
+    let overlayPackageInfoComponent = page.locator('overlay-package-report');
+    await Expect(overlayPackageInfoComponent).toHaveAttribute('package-name', packageName);
+
+    // use the search bar to find the @angular/cli package
+    packageName = `@angular/cli`;
+    const searchBar = page.locator('[name=q]');
+    await searchBar.fill(packageName);
+
+    // find the list item that links to the desired package
+    const getSearchResultInDropDown = page.locator(`li[aria-label="${packageName}"]`);
+    await getSearchResultInDropDown.click();
+
+    // check that overlay's title has changed to @angular/cli
+    overlayPackageInfoComponent = page.locator('overlay-package-report');
+    await Expect(overlayPackageInfoComponent).toHaveAttribute('package-name', packageName);
+  });
+
+  test('detect url change when using the searh bar in npmjs.com', async ({ page }) => {
+    const packageName = '@ngneat/spectator';
+    const baseURL = `https://www.npmjs.com/`;
+
+    await page.goto(baseURL, {
+      waitUntil: 'domcontentloaded',
+    });
+
+    // use the search bar to find the@ngneat/spectator
+    const searchBar = page.locator('[name=q]');
+    await searchBar.fill(packageName);
+
+    // find the list item that links to the desired package
+    const getSearchResultInDropDown = page.locator(`li[aria-label="${packageName}"]`);
+    await getSearchResultInDropDown.click();
+
+    // check that overlay's title has changed to @angular/cli
+    const overlayPackageInfoComponent = page.locator('overlay-package-report');
+    await Expect(overlayPackageInfoComponent).toHaveAttribute('package-name', packageName);
+  });
 });
