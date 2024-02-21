267978eb49610766e19c45211f71e4d086ce959b
Baruch Odem <baruch.odem@checkmarx.com>
2024-02-21 10:43:12 +0200

    run snapshot tests in saperated step due errors

--- tests/real-examples/update-snapshots.js
+++ tests/real-examples/update-snapshots.js
@@ -3,7 +3,9 @@ import * as fetchSnapshots from './fetch-snapshots.js';
 import * as realExampleSnapshots from './real-examples.js';
 
 export default async () => {
-  console.log(EOL + 'restoring snapshots');
-  await Promise.all([fetchSnapshots.updateSnapshots(false), realExampleSnapshots.snapshotWebpages(false)]);
-  console.log('snapshot restored');
+  if (!process.env.SKIP_SNAPSHOT) {
+    console.log(EOL + 'restoring snapshots');
+    await Promise.all([fetchSnapshots.updateSnapshots(false), realExampleSnapshots.snapshotWebpages(false)]);
+    console.log('snapshot restored');
+  }
 };
